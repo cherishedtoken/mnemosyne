c38fbc8751bd289decbe1ef59896a36614120856
Chris Gonzalez <twelfthapostrophe@gmail.com>
2014-06-04 22:06:02 -0700

    -Added ‘add item’ screen to app, made main screen a list of previously entered items. -Added storyboard actions and outlets.

---
+++ MNEAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  MNEAddToDoItemViewController.h
+//  Mnemosyne
+//
+//  Created by Chris Gonzalez on 6/4/14.
+//  Copyright (c) 2014 Christopher Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNEAddToDoItemViewController : UIViewController
+
+@end
---
+++ Mnemosyne/MNEToDoListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MNEToDoListTableViewController.h
+//  Mnemosyne
+//
+//  Created by Chris Gonzalez on 6/4/14.
+//  Copyright (c) 2014 Christopher Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNEToDoListTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+@end
