1ba8eb29a83f999e8f5e600740bfb3dc0abe67a3
Chris Gonzalez <twelfthapostrophe@gmail.com>
2014-06-04 20:06:45 -0700

    Initial Commit

---
+++ Mnemosyne/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mnemosyne
+//
+//  Created by Chris Gonzalez on 6/4/14.
+//  Copyright (c) 2014 Christopher Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MNEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MNEAppDelegate class]));
+    }
+}
